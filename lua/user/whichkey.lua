7011266cc4647d94aadf294da419f3e1467ae166
Rami Daghlawi <rami.daghlawi@gmail.com>
2022-09-05 22:45:54 +0200

    added overseer whichkey

--- lua/user/whichkey.lua
+++ lua/user/whichkey.lua
@@ -29,49 +29,54 @@ lvim.builtin.which_key.mappings["s"]["i"] = { "<CMD>PickIconsInsert<CR>", "Pick 
 
 
 lvim.builtin.which_key.mappings["D"] = { "<CMD>lua require('nvim-docker').containers.list_containers()<CR>", "Docker" }
+lvim.builtin.which_key.mappings["o"] = {
+  name = "+Overseer",
+  t = { "<CMD>OverseerToggle<CR>", "Toggle" },
+  r = { "<CMD>OverseerRun<CR>", "Run" }
+}
 
 lvim.builtin.which_key.mappings["g"]["h"] = {
   name = "+Github",
-    c = {
-        name = "+Commits",
-        c = { "<cmd>GHCloseCommit<cr>", "Close" },
-        e = { "<cmd>GHExpandCommit<cr>", "Expand" },
-        o = { "<cmd>GHOpenToCommit<cr>", "Open To" },
-        p = { "<cmd>GHPopOutCommit<cr>", "Pop Out" },
-        z = { "<cmd>GHCollapseCommit<cr>", "Collapse" },
-    },
-    i = {
-        name = "+Issues",
-        p = { "<cmd>GHPreviewIssue<cr>", "Preview" },
-    },
-    l = {
-        name = "+Litee",
-        t = { "<cmd>LTPanel<cr>", "Toggle Panel" },
-    },
-    r = {
-        name = "+Review",
-        b = { "<cmd>GHStartReview<cr>", "Begin" },
-        c = { "<cmd>GHCloseReview<cr>", "Close" },
-        d = { "<cmd>GHDeleteReview<cr>", "Delete" },
-        e = { "<cmd>GHExpandReview<cr>", "Expand" },
-        s = { "<cmd>GHSubmitReview<cr>", "Submit" },
-        z = { "<cmd>GHCollapseReview<cr>", "Collapse" },
-    },
-    p = {
-        name = "+Pull Request",
-        c = { "<cmd>GHClosePR<cr>", "Close" },
-        d = { "<cmd>GHPRDetails<cr>", "Details" },
-        e = { "<cmd>GHExpandPR<cr>", "Expand" },
-        o = { "<cmd>GHOpenPR<cr>", "Open" },
-        p = { "<cmd>GHPopOutPR<cr>", "PopOut" },
-        r = { "<cmd>GHRefreshPR<cr>", "Refresh" },
-        t = { "<cmd>GHOpenToPR<cr>", "Open To" },
-        z = { "<cmd>GHCollapsePR<cr>", "Collapse" },
-    },
-    t = {
-        name = "+Threads",
-        c = { "<cmd>GHCreateThread<cr>", "Create" },
-        n = { "<cmd>GHNextThread<cr>", "Next" },
-        t = { "<cmd>GHToggleThread<cr>", "Toggle" },
-    },
+  c = {
+    name = "+Commits",
+    c = { "<cmd>GHCloseCommit<cr>", "Close" },
+    e = { "<cmd>GHExpandCommit<cr>", "Expand" },
+    o = { "<cmd>GHOpenToCommit<cr>", "Open To" },
+    p = { "<cmd>GHPopOutCommit<cr>", "Pop Out" },
+    z = { "<cmd>GHCollapseCommit<cr>", "Collapse" },
+  },
+  i = {
+    name = "+Issues",
+    p = { "<cmd>GHPreviewIssue<cr>", "Preview" },
+  },
+  l = {
+    name = "+Litee",
+    t = { "<cmd>LTPanel<cr>", "Toggle Panel" },
+  },
+  r = {
+    name = "+Review",
+    b = { "<cmd>GHStartReview<cr>", "Begin" },
+    c = { "<cmd>GHCloseReview<cr>", "Close" },
+    d = { "<cmd>GHDeleteReview<cr>", "Delete" },
+    e = { "<cmd>GHExpandReview<cr>", "Expand" },
+    s = { "<cmd>GHSubmitReview<cr>", "Submit" },
+    z = { "<cmd>GHCollapseReview<cr>", "Collapse" },
+  },
+  p = {
+    name = "+Pull Request",
+    c = { "<cmd>GHClosePR<cr>", "Close" },
+    d = { "<cmd>GHPRDetails<cr>", "Details" },
+    e = { "<cmd>GHExpandPR<cr>", "Expand" },
+    o = { "<cmd>GHOpenPR<cr>", "Open" },
+    p = { "<cmd>GHPopOutPR<cr>", "PopOut" },
+    r = { "<cmd>GHRefreshPR<cr>", "Refresh" },
+    t = { "<cmd>GHOpenToPR<cr>", "Open To" },
+    z = { "<cmd>GHCollapsePR<cr>", "Collapse" },
+  },
+  t = {
+    name = "+Threads",
+    c = { "<cmd>GHCreateThread<cr>", "Create" },
+    n = { "<cmd>GHNextThread<cr>", "Next" },
+    t = { "<cmd>GHToggleThread<cr>", "Toggle" },
+  },
 }
