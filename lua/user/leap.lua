f7b28122f09037ae9fa549d9aa58a12220714093
Rami Daghlawi <rami.daghlawi@gmail.com>
2022-10-01 21:26:14 +0200

    added plugin configs

--- lua/user/leap.lua
+++ lua/user/leap.lua
@@ -3,23 +3,5 @@ if not status_ok then
   return
 end
 
-leap.setup {
-  max_aot_targets = nil,
-  highlight_unlabeled = false,
-  case_sensitive = false,
-  -- Sets of characters that should match each other.
-  -- Obvious candidates are braces and quotes ('([{', ')]}', '`"\'').
-  equivalence_classes = { ' \t\r\n', },
-  -- Leaving the appropriate list empty effectively disables "smart" mode,
-  -- and forces auto-jump to be on or off.
-  safe_labels = { . . . },
-  labels = { . . . },
-  -- These keys are captured directly by the plugin at runtime.
-  special_keys = {
-    repeat_search = '<enter>',
-    next_match    = '<enter>',
-    prev_match    = '<tab>',
-    next_group    = '<space>',
-    prev_group    = '<tab>',
-  },
-}
+leap.set_default_keymaps()
+leap.opts.ignore_case = true
