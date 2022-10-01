9cc261addcd00c2bc3af5f763d52fff016f706da
Rami Daghlawi <rami.daghlawi@gmail.com>
2022-10-01 21:40:06 +0200

    added spectre to whichkey

--- lua/user/whichkey.lua
+++ lua/user/whichkey.lua
@@ -129,3 +129,11 @@ lvim.builtin.which_key.mappings["o"] = {
   t = { "<CMD>OverseerToggle[!]<CR>", "Toggle" },
   r = { "<CMD>OverseerRun<CR>", "Run" }
 }
+
+-- replace
+lvim.builtin.which_key.mappings["r"] = {
+	name = "Replace",
+	r = { "<cmd>lua require('spectre').open()<cr>", "Replace" },
+	w = { "<cmd>lua require('spectre').open_visual({select_word=true})<cr>", "Replace Word" },
+	f = { "<cmd>lua require('spectre').open_file_search()<cr>", "Replace Buffer" },
+}
